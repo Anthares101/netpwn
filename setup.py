6010742f6294434c7590284a258bc8b8babeb5ec
Ángel <33127189+anthares101@users.noreply.github.com>
2021-08-01 04:39:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netpwn',
-    version='1.0.post3',
+    version='1.0.post4',
     license='GPL-2.0',
     author="Ángel Heredia",
     packages=find_packages(exclude=("*tests*",)),
